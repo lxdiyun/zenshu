9125e163a7f1ff6c631e4656ffe90a88726f1851
Adrain Lee <lxdiyun@gmail.com>
2013-03-01 13:22:38 +0800

    Add check script for transport

--- transport.py
+++ transport.py
@@ -1,6 +1,7 @@
 from django.db import models
 from zenshu.models import Book, Donator
 from django.utils.encoding import smart_str
+from django.db.models import Sum
 
 
 class ZshBook(models.Model):
@@ -31,69 +32,130 @@ class ZshPresent(models.Model):
         db_table = u'zsh_present'
 
 
+def pre_trans():
+#    presents = ZshPresent.objects.all().order_by('id')
+#    for pt in presents:
+#        pt.present_name = pt.present_name.strip()
+#        if pt.info is not None:
+#            pt.info = pt.info.strip()
+#        if pt.contact2 is not None:
+#            pt.contact2 = pt.contact2.strip()
+#        pt.save()
+#        print(pt.id)
+
+    zbks = ZshBook.objects.all().order_by('id')
+    for zbk in zbks:
+        zbk.bookname = zbk.bookname.strip()
+        if zbk.present_name is not None:
+            zbk.present_name = zbk.present_name.strip()
+#        if zbk.publisher is not None:
+#            zbk.publisher = zbk.publisher.strip()
+#        if zbk.author is not None:
+#            zbk.author = zbk.author.strip()
+#        if zbk.info is not None:
+#            zbk.info = zbk.info.strip()
+        zbk.save()
+        print(zbk.id)
+
+
 def trans():
     presents = ZshPresent.objects.all()
 
     for pt in presents:
-        dn = Donator()
-        dn.name = pt.present_name.strip()
-        if pt.info is not None:
-            dn.description = pt.info.strip()
-        if pt.contact2 is not None:
-            dn.contact_info = pt.contact2.strip()
-        dn.save()
+        pt_name = pt.present_name
+        dns = Donator.objects.filter(name=pt_name)
+        if (0 == dns.count()):
+            dn = Donator()
+            dn.name = pt_name
+            if pt.info is not None:
+                dn.description = pt.info
+            if pt.contact2 is not None:
+                dn.contact_info = pt.contact2
+            dn.save()
 
-        zshbooks = ZshBook.objects.filter(present_name=pt.present_name)
+            zshbooks = ZshBook.objects.filter(
+                present_name=pt_name)
 
-        for zbk in zshbooks:
-            bk = Book()
-            bk.name = zbk.bookname.strip()
-            if pt.sent_time is not None:
-                bk.donate_date = pt.sent_time
-            else:
-                bk.donate_date = '1990-02-28'
-            if zbk.vols:
-                bk.amount = zbk.vols
-            else:
-                bk.amount = 1
-            if zbk.author is not None:
-                bk.author_name = zbk.author.strip()
-            if zbk.info is not None:
-                bk.description = zbk.info.strip()
-            if zbk.publisher is not None:
-                bk.publisher = zbk.publisher.strip()
-            bk.publish_date = zbk.public_date
-            bk.save()
-            bk.donator.add(dn)
-            bk.save()
-            print bk.id
+            for zbk in zshbooks:
+                bk = Book()
+                bk.name = zbk.bookname
+                if pt.sent_time is not None:
+                    bk.donate_date = pt.sent_time
+                else:
+                    bk.donate_date = '1990-02-28'
+                if zbk.vols:
+                    bk.amount = zbk.vols
+                else:
+                    bk.amount = 1
+                if zbk.author is not None:
+                    bk.author_name = zbk.author
+                if zbk.info is not None:
+                    bk.description = zbk.info
+                if zbk.publisher is not None:
+                    bk.publisher = zbk.publisher
+                if zbk.info is not None:
+                    bk.description = zbk.info
+                bk.publish_date = zbk.public_date
+                bk.save()
+                bk.donator.add(dn)
+                bk.save()
+                print bk.id
+        else:
+            print("%d, %s" % (pt.id, smart_str(pt_name)))
 
 
 def trans2():
     zshbooks = ZshBook.objects.all()
 
     for zbk in zshbooks:
-        dns = Donator.objects.filter(name=zbk.present_name.strip())
+        dns = Donator.objects.filter(name=zbk.present_name)
         if (0 == dns.count()):
-            print("%d %s\n", zbk.id, smart_str(zbk.bookname))
-            bk = Book()
-            bk.name = zbk.bookname.strip()
-            bk.donate_date = '1990-02-28'
-            if zbk.vols:
-                bk.amount = zbk.vols
-            else:
-                bk.amount = 1
-            if zbk.author is not None:
-                bk.author_name = zbk.author.strip()
-            if zbk.info is not None:
-                bk.description = zbk.info.strip()
-            if zbk.publisher is not None:
-                bk.publisher = zbk.publisher.strip()
-            bk.publish_date = zbk.public_date
-            bk.save()
-
+            print("%d %s\n" % (zbk.id, smart_str(zbk.bookname)))
             dn = Donator()
-            dn.name = zbk.present_name.strip()
+            dn.name = zbk.present_name
             dn.save()
-            dn.book_set.add(bk)
-            dn.save()
+            bk = Book()
+
+            all_book_with_this_present = ZshBook.objects.filter(
+                present_name=zbk.present_name)
+
+            for zshbook in all_book_with_this_present:
+                bk = Book()
+                bk.name = zshbook.bookname
+                bk.donate_date = '1990-02-28'
+                if zshbook.vols:
+                    bk.amount = zbk.vols
+                else:
+                    bk.amount = 1
+                if zshbook.author is not None:
+                    bk.author_name = zshbook.author
+                if zshbook.info is not None:
+                    bk.description = zshbook.info
+                if zshbook.publisher is not None:
+                    bk.publisher = zshbook.publisher
+                bk.publish_date = zshbook.public_date
+                bk.save()
+                bk.donator.add(dn)
+                bk.save()
+                print(bk.id)
+
+
+def check():
+    zshbooks = ZshBook.objects.all()
+
+    for zbk in zshbooks:
+        bks = Book.objects.filter(name=zbk.bookname.strip())
+
+        if (0 == bks.count()):
+            print("%d %s\n" % (zbk.id, smart_str(zbk.bookname)))
+
+
+def check2():
+    pass
+
+
+def check3():
+    dns = Donator.objects.all()
+    for dn in dns:
+        total = dn.book_set.count()
+        print total
